0939bc46d0a9798382a9bb54436d42c547857223
DyatkoGleb <glebikon@yandex.ru>
2022-07-03 10:25:22 +0300

    hotfix

--- src/controllers/DailyPlaylistController.js
+++ src/controllers/DailyPlaylistController.js
@@ -7,7 +7,7 @@ class DailyPlaylistController {
         const currentYear = date.getFullYear()
         const currentMonth = date.getMonth() + 1
         const currentDay = date.getDate()
-        const isTodayPlaylistExists = await DailyPlaylist.find({ date: { $gte: currentYear + '-' + currentMonth + '-' + currentDay } })
+        const isTodayPlaylistExists = (await DailyPlaylist.find({ date: { $gte: currentYear + '-' + currentMonth + '-' + currentDay } })).length
 
 
         if (! isTodayPlaylistExists) {
@@ -15,6 +15,12 @@ class DailyPlaylistController {
 
             await dayliPlaylist.save()
         }
+    }
+
+    async getLastDailyPlaylist(req, res) {
+        const playlist = (await DailyPlaylist.find().limit(1).sort({$natural:-1}))[0]
+
+        return res.json({ data: playlist})
     }
 }
 
